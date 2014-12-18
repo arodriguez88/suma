fe24675ca80a6f86e639a973526990879f9e214c
prunex <prunex@acer>
2014-12-18 21:09:36 +0100

    //test1

---
+++ src/suma/Suma.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suma;
+
+/**
+ *
+ * @author prunex
+ */
+public class Suma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double a=4, b=9;
+		System.out.println("La suma es: "+(a+b));
+		System.out.println("La diferencia es: "+(a-b));
+		System.out.println("El producto es: "+(a*b));
+		System.out.println("La division es: "+(a/b));
+    }
+    
+}
